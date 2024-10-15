ba14a1c185f5786315e6b9928fab108494225ede
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-14 21:00:04 -0400

    Excersise 1 and Excersise 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace SelectionStatementExercise
 {
-    public class Program
+    public class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 4; 
+            Console.WriteLine("Guess my favorite number:");
+            int userInput = int.Parse(Console.ReadLine());
+            
+            while (userInput != favNumber)
+            {
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low. Try again:");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high. Try again:");
+                }
+                
+                userInput = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Congratulations! You guessed the correct number: " + favNumber);
         }
     }
 }
